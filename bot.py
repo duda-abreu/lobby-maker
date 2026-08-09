2ac1947f040baf691a2c7731f6574c1b65604e82
duda <mariaedrdabreu@gmail.com>
2026-08-09 13:38:09 -0300

    correçao

--- bot.py
+++ bot.py
@@ -3,7 +3,9 @@ import random
 import discord
 from discord import app_commands
 from discord.ext import commands
+from dotenv import load_dotenv
 
+load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
